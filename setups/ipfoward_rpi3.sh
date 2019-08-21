22dbc24216bb2a4400d41eb9545be5f4b81907b6
tareeqav <53956168+tareeqav@users.noreply.github.com>
2019-08-21 12:25:46 -0400

    Update ipfoward_rpi3.sh

--- setups/ipfoward_rpi3.sh
+++ setups/ipfoward_rpi3.sh
@@ -1,3 +1,9 @@
+#Front Right Camera
 iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
 iptables -A FORWARD -i wlan0 -o usb0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A FORWARD -i usb0 -o wlan0 -j ACCEPT
+
+#Front Left Camera
+iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
+iptables -A FORWARD -i wlan0 -o usb1 -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i usb1 -o wlan0 -j ACCEPT
