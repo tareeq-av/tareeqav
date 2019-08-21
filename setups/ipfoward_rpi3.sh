6bd9e0e3a3f7c09518574c50802ff60ca32f1bef
tareeqav <53956168+tareeqav@users.noreply.github.com>
2019-08-21 12:11:14 -0400

    Update ipfoward_rpi3.sh

--- setups/ipfoward_rpi3.sh
+++ setups/ipfoward_rpi3.sh
@@ -1,3 +1,3 @@
-iptables -t nat -A POSTROUTING -o wlp2s0 -j MASQUERADE
-iptables -A FORWARD -i wlp2s0 -o enp0s20f0u2c2 -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i enp0s20f0u2c2 -o wlp2s0 -j ACCEPT
+iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
+iptables -A FORWARD -i wlan0 -o usb0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i usb0 -o wlan0 -j ACCEPT
