0688b0bc469e15a0776ddeee68b6114e7997dc0b
Sameh Mohamed <smohamed1@lenovo.com>
2019-08-24 02:51:26 -0400

    successfully working mesh network with connectiving over ipv4 addresses as usual

--- setups/install-batman-adv.sh
+++ setups/install-batman-adv.sh
@@ -5,6 +5,5 @@ echo 'batman-adv' | sudo tee --append /etc/modules
 # Prevent DHCPCD from automatically configuring wlan0, THIS IS KEY
 echo 'denyinterfaces wlan0' | sudo tee --append /etc/dhcpcd.conf
 # Enable interfaces on boot
-echo "$(pwd)/start-batman-adv.sh" >> ~/.bashrc
 # And done!
 echo "Reboot to complete install."
